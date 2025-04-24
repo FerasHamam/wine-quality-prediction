c58dc8a90897587ce6af6b145353ad66bac9c554
FerasHamam <feras.i.hamam@gmail.com>
2025-04-24 12:37:53 -0400

    Added install docker script

---
+++ scripts/install-docker.sh
@@ -0,0 +1,28 @@
+# Update package index
+sudo apt-get update
+
+# Install dependencies
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Add your user to the docker group to run commands without sudo
+sudo usermod -aG docker $USER
+
+# Apply group changes to current session
+newgrp docker
